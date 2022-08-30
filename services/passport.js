3e84b66f8851deafb81a8a74e8b219d3bdff8666
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-30 15:25:18 -0700

    half way from mongodb google oauth set up

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,27 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
+const User = mongoose.model("users");
+//fetching user into model
+
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log("accessToken", accessToken);
-        console.log("refresh token", refreshToken);
-        console.log("profile", profile);
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+        } else {
+          //we dont have a user record with this ID, make a new record
+          new User({ googleId: profile.id }).save();
+        }
+      });
+    }
+  )
+);
